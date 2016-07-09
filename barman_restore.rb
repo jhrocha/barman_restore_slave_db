c967be1af3d37a7026a55cdfe35c93648856b90b
Jorge Henrique S. Rocha <jhrocha.sousa@gmail.com>
2016-07-09 03:00:49 -0300

    Update barman_restore.rb

--- barman_restore.rb
+++ barman_restore.rb
@@ -5,7 +5,8 @@ Open3.popen3(cmd) do |stdin, stdout, stderr, wait_thr|
   backup_list= stdout.read
 
   latestVersionID= backup_list.split[1]
-
+  
+  # The path to the postgres may be different. In my case, is below
   cmd= "barman recover --remote-ssh-command 'ssh your_user@host' YOUR_DB_IDENTIFIER /var/lib/postgresql/9.3/main/"
 
   if  backup_list.split("\n").first.include? "FAILED"
